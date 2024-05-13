c4f65ce6c5cee0a54d5e081f38a5689c4c31263e
ArturoRubin <116040655+ArturoRubin@users.noreply.github.com>
2024-05-13 16:29:22 -0600

    mejora en calendario dinamico

--- src/app/Pages/detalles-bauer/detalles-bauer.component.ts
+++ src/app/Pages/detalles-bauer/detalles-bauer.component.ts
@@ -1,9 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import { DateRange, MatDatepickerModule, MAT_RANGE_DATE_SELECTION_MODEL_PROVIDER } from '@angular/material/datepicker';
 import { MatCardModule } from '@angular/material/card';
 import { provideNativeDateAdapter } from '@angular/material/core';
-import { MatDialog } from '@angular/material/dialog';
+import { FormsModule } from '@angular/forms';
+import { ThisReceiver } from '@angular/compiler';
+
 
 
 
@@ -11,20 +13,37 @@ import { MatDialog } from '@angular/material/dialog';
 @Component({
   selector: 'app-detalles-bauer',
   standalone: true,
-  providers: [provideNativeDateAdapter()],
+  providers: [provideNativeDateAdapter(), MAT_RANGE_DATE_SELECTION_MODEL_PROVIDER],
   imports: [
     RouterLink,
     RouterLinkActive,
     MatCardModule,
     MatDatepickerModule,
-
+    FormsModule
   ],
+
   templateUrl: './detalles-bauer.component.html',
   styleUrl: './detalles-bauer.component.css'
 })
-export class DetallesBauerComponent {
-  selected: Date | null | undefined;
+export class DetallesBauerComponent implements OnInit {
 
+  currentDate = new Date();
+  futureDate = new Date(this.currentDate.getTime());
+  selected : any;
+
+
+  /*  */
+ /*  selected : DateRange< Date > = { 
+    start:  new Date
+  }; */
+  ngOnInit(): void {
+    this.futureDate.setDate(this.currentDate.getDate() + 7);
+    /* this.selected = { 
+      start: this.currentDate,
+      end: this.futureDate,
+    } */
+    this.selected = new DateRange(this.currentDate, this.futureDate);
+  }
 }
 
 
